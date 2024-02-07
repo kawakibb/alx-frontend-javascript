2fc3757d2febd6c87bb421d920e33d5be1f76718
kawakibb <kawakibk7@gmail.com>
2024-02-07 22:46:59 +0100

    error fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,18 +2,24 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-	  for (const item of values) {
-	        if (item.status === 'rejected') {
-		arr.push({ status: item.status, value: item.reason });
-	      } else {
-	      arr.push({ status: item.status, value: item.value });
-            }
-	    }
-	      return arr;
-	    });
+	  return Promise.allSettled([
+		      signUpUser(firstName, lastName),
+		      uploadPhoto(fileName)])
+	    .then((result) => {
+		          const array = [];
+		          result.forEach((index) => {
+				          if (index.status === 'fulfilled') {
+						            array.push({
+								                status: index.status,
+								                value: index.value,
+								              });
+						          } else {
+								            array.push({
+										                status: index.status,
+										                value: `Error: ${index.reason.message}`,
+										              });
+								          }
+				        });
+		          return array;
+		        });
 }

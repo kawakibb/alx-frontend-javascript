747b57d50db630cac17c6951a1f282b36b31f7a6
kawakibb <kawakibk7@gmail.com>
2024-02-13 23:58:21 +0100

    main

--- 0x03-ES6_data_manipulation/tests/1-main.js
+++ 0x03-ES6_data_manipulation/tests/1-main.js
@@ -2,8 +2,4 @@ import getListStudentIds from "./1-get_list_student_ids.js";
 import getListStudents from "./0-get_list_students.js";
 
 console.log(getListStudentIds("hello"));
-console.log(getListStudentIds(getListStudents()));import getListStudentIds from "./1-get_list_student_ids.js";
-import getListStudents from "./0-get_list_students.js";
-
-console.log(getListStudentIds("hello"));
 console.log(getListStudentIds(getListStudents()));

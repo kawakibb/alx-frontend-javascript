cad5257d56dd152c1c4f5600d1e7aba6bbaa9bb0
kawakibb <kawakibk7@gmail.com>
2024-02-15 22:53:41 +0100

    error fixed

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 export default function groceriesList() {
-	  const items = new Map();
-	  items.set('Apples', 10);
-	  items.set('Tomatoes', 10);
-	  items.set('Pasta', 1);
-	  items.set('Rice', 1);
-	  items.set('Banana', 5);
+      const items = new Map();
+      items.set('Apples', 10);
+      items.set('Tomatoes', 10);
+      items.set('Pasta', 1);
+      items.set('Rice', 1);
+      items.set('Banana', 5);
 
-	  return items;
+      return items;
 }
 
